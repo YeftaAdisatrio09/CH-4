8125d759b815ea7430e7b4e87037c4c3f3461f1b
Yefta <yeftaadisatrio@gmail.com>
2022-04-11 18:48:11 +0700

    header

---
+++ src/Components/Footer/Footer.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+import test from "./footer.module.css";
+
+const Footer = () => {
+    return <></>;
+};
+
+export default Footer;
---
+++ src/Components/Header/Header.js
@@ -0,0 +1,29 @@
+import React from "react";
+
+import test from "./header.module.css";
+
+const Header = () => {
+    return (
+        <div className={test.nav}>
+            <ul>
+                <li>
+                    <a href="#home">Our Service</a>
+                </li>
+                <li>
+                    <a href="#news">Why Us</a>
+                </li>
+                <li>
+                    <a href="#contact">Testimonial</a>
+                </li>
+                <li>
+                    <a href="#about">FAQ</a>
+                </li>
+                <li>
+                    <button>Registrer</button>
+                </li>
+            </ul>
+        </div>
+    );
+};
+
+export default Header;
